02d06629cb9cb7a178aa15a1e2ffe919de9e4445
Sebastian Servin <servinseb@hotmail.com>
2022-05-09 15:48:50 -0600

    Progress on users

--- frontend/src/Monitor.js
+++ frontend/src/Monitor.js
@@ -1,5 +1,5 @@
 import './css/App.css'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from './components/NavBar'
 import TableDisplay from './components/TableDisplay';
 import Add from './components/crud/Add';
@@ -8,69 +8,115 @@ import Register from './components/account/Register';
 import IndividualSite from './components/IndividualSite'
 import PrivateRoute from './components/account/PrivateRoute';
 import { HashRouter as Router, Route, Routes, Redirect } from "react-router-dom";
-import loadUser from './components/actions/auth';
+import { LoadUser } from './components/actions/auth';
 
 
-class Monitor extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentPage: 'Alerts',
-      individualSite: null,
-      user: null,
-      userType: null
-    }
+function Monitor() {
+  const [currentPage, setCurrentPage] = useState('Alerts');
+  const [individualSite, setIndividualSite] = useState(null);
+  const [user, setUser] = useState();
+  const [isAuthenticated, setIsAuthenticated] = useState();
+  const loadUser = LoadUser();
+  
+  useEffect(() => {
+    setUser(loadUser.returnObject);
+    setIsAuthenticated(loadUser.isAuthenticated);
+    document.body.style.backgroundColor = "#FAFAFA";
+  }, []);
+  
+  function updatePage(curr, site) {
+    setCurrentPage(curr);
+    setIndividualSite(site);
   }
-
-  // Handles content when clicking navbar
-  updatePage = (curr, site) => {
-    this.setState({
-      currentPage: curr,
-      individualSite: site
-    });
-  }
-
-  getPageOption = (option) => {
+  
+  function getPageOption(option) {
     if (option === "Status" || option === "SSL Certificates" || option === "Alerts") {
-      return <TableDisplay option={option} updatePage={this.updatePage}/>;
+      return <TableDisplay option={option} updatePage={updatePage}/>;
     } else if (option === "Add") {
-      return <Add returnToStatus={this.updatePage}/>;
+      return <Add returnToStatus={updatePage}/>;
     } else if (option === "Individual Site") {
-      return <IndividualSite returnToStatus={this.updatePage} siteID={this.state.individualSite}/>
+      return <IndividualSite returnToStatus={updatePage} siteID={this.state.individualSite}/>
     } else {
       return <h1>Not Found</h1>
     }
   }
-
-  componentDidMount = () => {
-    document.body.style.backgroundColor = "#FAFAFA"
-    const userAuth = loadUser()
-    console.log(userAuth)
-  }
-
-  getUserData = () => {
-    
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <NavBar updatePage={this.updatePage}/>
-          <div className='container'>
-            <Routes>
-              <Route exact path="/" element={ <PrivateRoute isLoading={false} isAuthenticated={false} currentPage={this.getPageOption(this.state.currentPage)} /> } />
-              <Route exact path="/register" element={ <Register/> } />
-              <Route exact path="/login" element={ <Login/> } />
-            </Routes>
-          </div>
-          <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossOrigin="anonymous"></script>
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossOrigin="anonymous"></script>
+  console.log(isAuthenticated)
+  
+  return (
+    <Router>
+      <div className="App">
+        <NavBar updatePage={updatePage}/>
+        <div className='container'>
+          <Routes>
+            <Route exact path="/" element={ <PrivateRoute isLoading={false} isAuthenticated={isAuthenticated} currentPage={getPageOption(currentPage)} /> } />
+            <Route exact path="/register" element={ <Register/> } />
+            <Route exact path="/login" element={ <Login/> } />
+          </Routes>
         </div>
-      </Router>
-    );
-  }
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossOrigin="anonymous"></script>
+      </div>
+    </Router>
+  );
 }
+
+// class Monitor extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       currentPage: 'Alerts',
+//       individualSite: null,
+//       user: null,
+//       userType: null
+//     }
+//   }
+
+  // Handles content when clicking navbar
+  // updatePage = (curr, site) => {
+  //   this.setState({
+  //     currentPage: curr,
+  //     individualSite: site
+  //   });
+  // }
+
+  // getPageOption = (option) => {
+  //   if (option === "Status" || option === "SSL Certificates" || option === "Alerts") {
+  //     return <TableDisplay option={option} updatePage={this.updatePage}/>;
+  //   } else if (option === "Add") {
+  //     return <Add returnToStatus={this.updatePage}/>;
+  //   } else if (option === "Individual Site") {
+  //     return <IndividualSite returnToStatus={this.updatePage} siteID={this.state.individualSite}/>
+  //   } else {
+  //     return <h1>Not Found</h1>
+  //   }
+  // }
+
+  // componentDidMount = () => {
+  //   document.body.style.backgroundColor = "#FAFAFA"
+  // }
+
+  // getUserData = () => {
+    
+  // }
+
+  // render() {
+  //   return (
+  //     <Router>
+  //       <div className="App">
+  //         <NavBar updatePage={this.updatePage}/>
+  //         <div className='container'>
+  //           <Routes>
+  //             <Route exact path="/" element={ <PrivateRoute isLoading={false} isAuthenticated={false} currentPage={this.getPageOption(this.state.currentPage)} /> } />
+  //             <Route exact path="/register" element={ <Register/> } />
+  //             <Route exact path="/login" element={ <Login/> } />
+  //           </Routes>
+  //         </div>
+  //         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossOrigin="anonymous"></script>
+  //         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossOrigin="anonymous"></script>
+  //       </div>
+  //     </Router>
+  //   );
+  // }
+// }
 
 export default Monitor;
--- frontend/src/components/actions/auth.js
+++ frontend/src/components/actions/auth.js
@@ -1,33 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { USER_ERROR, USER_LOADED, USER_LOADING } from './types';
 
 
-const loadUser = async (token) => {
-    const toReturn = {
-        "returnObject": null,
-        "returnType": null
-    };
+export const LoadUser = (token) => {
+    const [returnObject, setReturnObject] = useState();
+    const [isAuthenticated, setIsAuthenticated] = useState();
     const config = {
         headers: {
             "Content-type": "application/json"
         }
     };
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const res = await axios.get("http://localhost:8000/api/auth/user", config);
+                setReturnObject(res.data)
+                setIsAuthenticated(true);
+            } catch (err) {                
+                setReturnObject(err.response.data)
+                setIsAuthenticated(true);
+            }
+        }
+        getData();
+    }, []);
 
-    if (token) {
-        config.headers["Authorization"] = `Token ${token}`
-    }
-    try {
-        const res = await axios.get('http://localhost:8000/api/auth/user', config)
-        // toReturn =  {returnObject: res.data, returnType: USER_LOADED};
-        toReturn["returnObject"] = res.data;
-        toReturn["returnType"] = USER_LOADED; 
-    } catch (error) {
-        // toReturn =  {returnObject: error.response.data, returnType: USER_ERROR};
-        toReturn["returnObject"] = error.response.data;
-        toReturn["returnType"] = USER_ERROR; 
-    }
-    return toReturn;
+    return {returnObject, isAuthenticated};
 }
 
-export default loadUser;
+// export const LoadUser = (token) => {
+//     const [returnObject, setReturnObject] = useState();
+//     const [isAuthenticated, setIsAuthenticated] = useState();
+//     const config = {
+//         headers: {
+//             "Content-type": "application/json"
+//         }
+//     };
+//     useEffect(() => {
+//         axios.get("http://localhost:8000/api/auth/user", config)
+//         .then((res) => {
+//             setReturnObject(res.data);
+//             setIsAuthenticated(true);
+//         })
+//         .catch((err) => {
+//             setReturnObject(err.response.data);
+//             setIsAuthenticated(true)
+//         });
+//     }, []);
+
+//     if (returnObject === undefined && isAuthenticated === undefined) {
+//         return {returnObject: null, isAuthenticated: null}; 
+//     } else {
+//         return {returnObject, isAuthenticated};
+//     }
+// }
